b7dc9c347de0e63be050e574eba12a889d8e255e
long2ice <long2ice@gmail.com>
2022-11-26 21:33:29 +0800

    feat: digitalvirt add datacenter

--- tests/providers/test_digitalvirt.py
+++ tests/providers/test_digitalvirt.py
@@ -4,3 +4,8 @@ from vpsmon.providers.digitalvirt import DigitalVirt
 async def test_get_vps_list():
     vps_list = await DigitalVirt.get_vps_list()
     assert len(vps_list) > 0
+
+
+async def test_get_datacenter_list():
+    datacenters = await DigitalVirt.get_datacenter_list()
+    assert len(datacenters) > 0
--- vpsmon/providers/digitalvirt.py
+++ vpsmon/providers/digitalvirt.py
@@ -1,9 +1,14 @@
 import asyncio
 import itertools
+import json
 import re
+from typing import Optional
+from urllib.parse import urlparse
 
 import httpx
 from requests_html import HTML
+from websockets import connect  # type: ignore
+from websockets.exceptions import InvalidStatusCode
 
 from vpsmon.enums import ProviderType
 from vpsmon.models import VPS, DataCenter
@@ -30,7 +35,9 @@ class DigitalVirt(Provider):
             for div in html.find("div.product-wrap-box"):  # type: ignore
                 name = div.find(".product-wrap-title", first=True).text
                 price = (
-                    div.find(".product-wrap-price .big", first=True).text.replace("¥", "").strip()
+                    div.find(".product-wrap-price .big", first=True)
+                    .text.replace("¥", "")
+                    .strip()
                 )
                 won = div.find(".product-wrap-price .won", first=True).text
                 period = "month"
@@ -100,20 +107,70 @@ class DigitalVirt(Provider):
     @classmethod
     async def get_vps_list(cls) -> list[VPS]:
         tasks = [
-            asyncio.ensure_future(cls._get_vps_list(f"{cls.homepage}/store/la-vps", "洛杉矶 9929")),
+            asyncio.ensure_future(
+                cls._get_vps_list(f"{cls.homepage}/store/la-vps", "洛杉矶 9929")
+            ),
             asyncio.ensure_future(
                 cls._get_vps_list(f"{cls.homepage}/store/la-vps-4837", "洛杉矶 4837")
             ),
-            asyncio.ensure_future(cls._get_vps_list(f"{cls.homepage}/store/qn-vps", "洛杉矶 QN")),
-            asyncio.ensure_future(cls._get_vps_list(f"{cls.homepage}/store/hk-cmi-vps", "香港 CMI")),
+            asyncio.ensure_future(
+                cls._get_vps_list(f"{cls.homepage}/store/qn-vps", "洛杉矶 QN")
+            ),
+            asyncio.ensure_future(
+                cls._get_vps_list(f"{cls.homepage}/store/hk-cmi-vps", "香港 CMI")
+            ),
             asyncio.ensure_future(
                 cls._get_vps_list(f"{cls.homepage}/store/la-vps-cn2gia", "洛杉矶 CN2 GIA")
             ),
-            asyncio.ensure_future(cls._get_vps_list(f"{cls.homepage}/store/jp-vps-bbetc", "日本软银")),
+            asyncio.ensure_future(
+                cls._get_vps_list(f"{cls.homepage}/store/jp-vps-bbetc", "日本软银")
+            ),
         ]
         vps_list = await asyncio.gather(*tasks)
         return list(itertools.chain(*vps_list))
 
     @classmethod
+    async def _get_datacenter_list(cls, path: str, name: str) -> Optional[DataCenter]:
+        url = f"{cls.homepage}/store{path}"
+        async with httpx.AsyncClient(http2=True, timeout=cls.timeout) as client:
+            res = await client.get(url)
+            text = res.text
+            datacenter_urls = re.findall(r"LookingGlass: (http://.+)</p>", text)
+            if not datacenter_urls:
+                return None
+            datacenter_url = datacenter_urls[0]
+            netloc = urlparse(datacenter_url).netloc
+            ws_url = f"ws://{netloc}/ws"
+            try:
+                async with connect(ws_url) as websocket:
+                    data = await websocket.recv()
+                    data = data.split("|")[1]
+                    data = json.loads(data)
+                    public_ipv4 = data["public_ipv4"]
+                    public_ipv6 = data["public_ipv6"] or ""
+                    location = data["location"]
+                    datacenter = DataCenter(
+                        provider=cls.type,
+                        name=name,
+                        ipv4=public_ipv4,
+                        ipv6=public_ipv6,
+                        location=location,
+                    )
+                    return datacenter
+            except InvalidStatusCode:
+                return None
+
+    @classmethod
     async def get_datacenter_list(cls) -> list[DataCenter]:
-        return []
+        tasks = [
+            asyncio.ensure_future(cls._get_datacenter_list("/la-vps", "洛杉矶 9929")),
+            asyncio.ensure_future(cls._get_datacenter_list("/la-vps-4837", "洛杉矶 4837")),
+            asyncio.ensure_future(cls._get_datacenter_list("/qn-vps", "洛杉矶 QN")),
+            asyncio.ensure_future(cls._get_datacenter_list("/hk-cmi-vps", "香港 CMI")),
+            asyncio.ensure_future(
+                cls._get_datacenter_list("/la-vps-cn2gia", "洛杉矶 CN2 GIA")
+            ),
+            asyncio.ensure_future(cls._get_datacenter_list("/jp-vps-bbetc", "日本软银")),
+        ]
+        datacenter_list = await asyncio.gather(*tasks)
+        return list(filter(None, datacenter_list))
